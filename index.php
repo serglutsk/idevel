1da610fd02933f3f164735f21c677a1c7a401ccc
serg <serg@serg-System-Product-Name.(none)>
2014-02-14 10:01:49 +0200

    перший commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+ class config{
+	  	const HOST="localhost";
+	  	const USER="root";
+	  	const PASS="";
+	  	const DB="web_guru";
+
+  }
+class startup extends config{
+static function connect(){
+ 	try{
+
+	        $db = new PDO('mysql:host='.self::HOST.';dbname='.self::DB,self::USER,self::PASS);
+         	//виставляємо роботу з помилками
+         	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+         	//встановлюємо кодування
+         	$db->query("SET NAMES utf8");
+			
+			}
+		catch(PDOException $e){
+			die("Error: ".$e->getMessage());
+	 }
+	return $db;
+  	}
+  }
+echo 'aaaa';
+for($i=0;$i<10;$i++) {
+		echo $i.'<br>';	
+	}
+?>
